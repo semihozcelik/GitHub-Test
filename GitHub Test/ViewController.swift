585a8501b84da8963f089a53ca3332314bb3ff16
Semih Özçelik <semih.ozcelik@penta.com.tr>
2018-06-13 12:40:59 +0300

    Initial Commit

---
+++ GitHub Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub Test
+//
+//  Created by Semih Ozcelik on 6/13/18.
+//  Copyright © 2018 BSS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
