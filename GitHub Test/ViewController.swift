3069c1d74e1bd8abb75e4e25acde1b553aa34b8b
Semih Özçelik <semih.ozcelik@penta.com.tr>
2018-06-13 12:51:40 +0300

    blue to yellow background

--- GitHub Test/ViewController.swift
+++ GitHub Test/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        view.backgroundColor = UIColor.blue
+        view.backgroundColor = UIColor.yellow
         
     }
     
